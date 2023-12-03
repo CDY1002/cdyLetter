161ef3cf23900bf7d4eb97a9b8f6950fc651dc75
CDY1002 <1019362738@qq.com>
2023-12-03 17:50:16 +0800

    更新了文字雨

---
+++ js/circleLayout.js
@@ -0,0 +1,54 @@
+// 创建SVG画布
+//var svg = d3.select("div")
+  //.append("svg")
+  //.attr("width", 2000)
+ // .attr("height", 2000);
+var svg_circleLayout = d3.select("#circleLayout")
+// 读取JSON文件
+d3.json("../data/letter1.1.json").then(data => {
+  // 计算每个对象在圆上的位置
+  const circleData = data.map((d, i) => {
+    const radius = d.r;
+    const textLength = d.letter.length;
+    const textAngle = 2 * Math.PI / textLength;
+
+    const letterData = d.letter.split("").map((letter, j) => {
+      const angle = j * textAngle;
+      const x = 500 + Math.cos(angle) * radius;
+      const y = 500 + Math.sin(angle) * radius;
+      return { letter, x, y,type:d.type }
+      ;
+
+    });
+
+    return letterData;
+  }).flat();
+
+  // 在SVG上绘制文字
+  const texts = svg_circleLayout.selectAll("text")
+    .data(circleData)
+    .enter()
+    .append("text")
+    .attr("x", d => d.x)
+    .attr("y", d => d.y)
+    .attr("text-anchor", "middle")
+    .attr("dominant-baseline", "middle")
+    .text(d => d.letter)
+    .style('font-size', '1.4px');//字号
+
+  // 监听筛选器变化
+  d3.select("#color-filter").on("change", function() {
+    const selectedColor = d3.select(this).property("value");
+
+    // 根据选中的颜色修改文字样式
+    texts.attr("fill", d => {
+      if (selectedColor === "粉色" && d.type === 1) {
+        return "pink";
+      } else if (selectedColor === "红色" && d.type === 2) {
+        return "red";
+      } else {
+        return "black";
+      }
+    });
+  });
+});
---
+++ js/letterRain.js
@@ -0,0 +1,60 @@
+var textNum = 600
+var minSpeed = 0.1
+var maxSpeed = 3.7
+
+var positions = []
+var speeds = []
+var texts = []
+var customFont;
+
+var textCandidates = ['我们', '现在', '你们', '没有', '大人', '不能', '母亲', '生活', '一切', '工作', '身体', '家中', '希望', '以后', '中国', '一定', '在外', '老人家', '痛苦', '家庭', '抗战', '社会','回家', '挂念', '收到', '革命', '为了', '家里' ,'读书', '国家', '情形','时代', '环境', '办法', '军阀', '父母', '困难']
+
+
+function preload() {
+	customFont = loadFont('../font/FZLTXHJW.TTF');
+  }
+
+function setup() {
+	background(255)
+	
+	let canvas = createCanvas(windowWidth, windowHeight);
+	canvas.parent('letterRain')
+
+	textFont(customFont);
+	
+	for (var i = 0; i < textNum; i++) {
+		positions.push({ x: random(0, windowWidth), y: random(0, -windowHeight*3) })
+		speeds.push(random(minSpeed, maxSpeed))
+		texts.push(textCandidates[Math.floor(random(1) * textCandidates.length)])
+	}
+	
+	textAlign(CENTER)
+	smooth()
+}
+
+function draw() {
+	clear()
+	
+	strokeWeight(2)
+	stroke(255)
+	
+	fill(0)
+	noStroke()
+	for (var i = 0; i<textNum; i++) {
+		
+		push()
+			translate(positions[i].x,  positions[i].y)
+			scale(2 * (speeds[i] - minSpeed)/(maxSpeed - minSpeed))
+			textFont(customFont);
+			text(texts[i], 0, 0)
+		pop()
+		
+		positions[i].y += speeds[i]
+		
+		if (positions[i].y > windowHeight + 100) { 
+			texts[i] = textCandidates[Math.floor(random(1) * textCandidates.length)]
+			speeds[i] = random(minSpeed, maxSpeed)
+			positions[i] = { x: random(0, windowWidth), y: random(0, -windowHeight*2) }
+		}
+	}
+}
